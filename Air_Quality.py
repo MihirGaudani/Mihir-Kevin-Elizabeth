0a1a88a7a98052f16c25be39b922244f8fc345e8
lol173821 <68711803+lol173821@users.noreply.github.com>
2025-02-14 14:44:33 -0800

    Update Air_Quality.py

--- Air_Quality.py
+++ Air_Quality.py
@@ -11,6 +11,7 @@ import board
 import busio
 from digitalio import DigitalInOut, Direction, Pull
 import adafruit_pm25
+import csv
 
 reset_pin = None
 # If you have a GPIO, its not a bad idea to connect it to the RESET pin
@@ -40,9 +41,9 @@ uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
  pm25 = PM25_UART(uart, reset_pin)
 
 # Create library object, use 'slow' 100KHz frequency!
-i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
+#i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
 # Connect to a PM2.5 sensor over I2C
-pm25 = PM25_I2C(i2c, reset_pin)
+#pm25 = PM25_I2C(i2c, reset_pin)
 
 print("Found PM2.5 sensor, reading data...")
 
@@ -77,3 +78,23 @@ while True:
     print("Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
     print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
     print("---------------------------------------")
+
+
+
+
+    file = open("data.csv", "w", newline=none)
+    file_writer = csv.writer(file)
+     file_writer.writerow(["time", "data1", "data2", "data3", "data4", "data5", "data6", "data7", "data8"])
+    print("Concentration Units (standard)")
+    print("---------------------------------------")
+    data1 = "PM 1.0: %d\tPM2.5: %d\tPM10: %d"  % (aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"])
+    data2 = "PM 1.0: %d\tPM2.5: %d\tPM10: %d" % (aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"])
+
+    data3 = "Particles > 0.3um / 0.1L air:", aqdata["particles 03um"]
+    data4 = "Particles > 0.5um / 0.1L air:", aqdata["particles 05um"])
+    data5 = "Particles > 1.0um / 0.1L air:", aqdata["particles 10um"])
+    data6 = "Particles > 2.5um / 0.1L air:", aqdata["particles 25um"])
+    data7 = "Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
+    data8 = "Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
+    time = time.time()
+    file_writer.writerow([time, data1, data2, data3, data4, data5, data6, data7, data8])
