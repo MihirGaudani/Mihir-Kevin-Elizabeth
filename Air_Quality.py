6347fb8135df74ec3729ad99bfe2ec29f2393416
lol173821 <68711803+lol173821@users.noreply.github.com>
2025-02-14 15:06:32 -0800

    Update Air_Quality.py

--- Air_Quality.py
+++ Air_Quality.py
@@ -44,7 +44,7 @@ pm25 = PM25_UART(uart, reset_pin)
 #i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
 # Connect to a PM2.5 sensor over I2C
 #pm25 = PM25_I2C(i2c, reset_pin)
-file = open("data.csv", "a", newline=None)
+file = open("data.csv", "w", newline=None)
 file_writer = csv.writer(file)
 file_writer.writerow(["time", "data1", "data2", "data3", "data4", "data5", "data6", "data7", "data8"])
 print("Found PM2.5 sensor, reading data...")
@@ -80,14 +80,14 @@ while True:
     print("Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
     print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
     print("---------------------------------------")
-    data1 = "PM 1.0: %d\tPM2.5: %d\tPM10: %d"  % (aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"])
-    data2 = "PM 1.0: %d\tPM2.5: %d\tPM10: %d" % (aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"])
-    data3 = "Particles > 0.3um / 0.1L air:", aqdata["particles 03um"]
-    data4 = "Particles > 0.5um / 0.1L air:", aqdata["particles 05um"]
-    data5 = "Particles > 1.0um / 0.1L air:", aqdata["particles 10um"]
-    data6 = "Particles > 2.5um / 0.1L air:", aqdata["particles 25um"]
-    data7 = "Particles > 5.0um / 0.1L air:", aqdata["particles 50um"]
-    data8 = "Particles > 10 um / 0.1L air:", aqdata["particles 100um"]
+    data1 = (aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"])
+    data2 = (aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"])
+    data3 = aqdata["particles 03um"]
+    data4 = aqdata["particles 05um"]
+    data5 = aqdata["particles 10um"]
+    data6 = aqdata["particles 25um"]
+    data7 = aqdata["particles 50um"]
+    data8 = aqdata["particles 100um"]
     time1 = time.time()
     file_writer.writerow([time1, data1, data2, data3, data4, data5, data6, data7, data8])
 
