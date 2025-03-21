5461407e47ea5df08fd2c2a6323e1f1895e6ac72
lol173821 <68711803+lol173821@users.noreply.github.com>
2025-03-21 15:44:58 -0700

    Update Radiation_Sensors.py

--- Radiation_Sensors.py
+++ Radiation_Sensors.py
@@ -1,31 +1,25 @@
 import RPi.GPIO as GPIO
- import datetime
- import time
- #write a script that prints out the time-stamp each time a count is detected by your sensor
- #NOTE: you will want to use a falling-edge event detection
- #NOTE: call-back methods are functions that only run when some external property changes, 
- #in this case, the change in voltage on the GPIO pin
- Count = 0
- def my_callback(channel):
-     if GPIO.input(channel) == GPIO.LOW:
-         print('\n▼  at ' + str(datetime.datetime.now()))
-         Count = Count + 1
-     else:
-         print('\n ▲ at ' + str(datetime.datetime.now())) 
-         Count = Count + 1
- seconds = time()
- thirds = 0
- while thirds > 120:
+import datetime
+import time
+#write a script that prints out the time-stamp each time a count is detected by your sensor
+#NOTE: you will want to use a falling-edge event detection
+#NOTE: call-back methods are functions that only run when some external property changes, 
+#in this case, the change in voltage on the GPIO pin
+global Count = 0
+def my_callback(channel):
+     print('\n▼  at ' + str(datetime.datetime.now()))
+     global Count
+     Count = Count + 1
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(6, GPIO.IN)
+GPIO.add_event_detect(6, GPIO.FALLING, callback=my_callback)
+seconds = time()
+thirds = 0
+while thirds > 120:
+    time.sleep(10)
+    thirds = thirds + 10
+    if thirds == 60:
+        print(count)
  
-     GPIO.setmode(GPIO.BCM)
-     GPIO.setup(6, GPIO.IN)
-     GPIO.add_event_detect(6, GPIO.BOTH, callback=my_callback)
-     if thirds == 60:
-         print(count)
- 
-     thirds = time() - seconds
- 
- finally:
-     GPIO.cleanup()
-  
- print("Goodbye!")
+print("Goodbye!")
