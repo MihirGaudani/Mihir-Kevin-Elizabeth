845aad08a4e50286e73ddf049be94ba8a20b44d7
lol173821 <68711803+lol173821@users.noreply.github.com>
2025-03-21 14:58:19 -0700

    Update Radiation_Sensors.py

--- Radiation_Sensors.py
+++ Radiation_Sensors.py
@@ -1,22 +1,26 @@
 import RPi.GPIO as GPIO
 import datetime
+import time
 #write a script that prints out the time-stamp each time a count is detected by your sensor
 #NOTE: you will want to use a falling-edge event detection
 #NOTE: call-back methods are functions that only run when some external property changes, 
 #in this case, the change in voltage on the GPIO pin
-
+Count = 0
 def my_callback(channel):
     if GPIO.input(channel) == GPIO.HIGH:
         print('\n▼  at ' + str(datetime.datetime.now()))
+        Count = Count + 1
     else:
         print('\n ▲ at ' + str(datetime.datetime.now())) 
- 
-try:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(6, GPIO.IN)
-    GPIO.add_event_detect(6, GPIO.BOTH, callback=my_callback)
- 
-    message = raw_input('\nPress any key to exit.\n')
+        Count = Count + 1
+seconds = time()
+thirds = 0
+while thirds > 60
+    try:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(6, GPIO.IN)
+        GPIO.add_event_detect(6, GPIO.BOTH, callback=my_callback)
+    thirds = time() - seconds
  
 finally:
     GPIO.cleanup()
