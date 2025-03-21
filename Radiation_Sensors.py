e0b6b7a2c1babc6d1af8719ecf0bcdef6942bb35
lol173821 <68711803+lol173821@users.noreply.github.com>
2025-03-21 14:49:20 -0700

    Update Radiation_Sensors.py

--- Radiation_Sensors.py
+++ Radiation_Sensors.py
@@ -1,15 +1,9 @@
 import RPi.GPIO as GPIO
 import datetime
 #write a script that prints out the time-stamp each time a count is detected by your sensor
-NOTE: you will want to use a falling-edge event detection
-NOTE: call-back methods are functions that only run when some external property changes, 
-in this case, the change in voltage on the GPIO pin
-
-channel = GPIO.wait_for_edge(channel, GPIO_RISING, timeout=5000)
-if channel is None:
-    print('Timeout occurred')
-else:
-    print('Edge detected on channel', channel)
+#NOTE: you will want to use a falling-edge event detection
+#NOTE: call-back methods are functions that only run when some external property changes, 
+#in this case, the change in voltage on the GPIO pin
 
 def my_callback(channel):
     if GPIO.input(channel) == GPIO.HIGH:
