5302ffbaad94cdb8e482379e63db361d4a4e86ad
lol173821 <68711803+lol173821@users.noreply.github.com>
2025-03-21 15:28:21 -0700

    Update Radiation_Sensors.py

--- Radiation_Sensors.py
+++ Radiation_Sensors.py
@@ -12,16 +12,18 @@ def my_callback(channel):
         Count = Count + 1
     else:
         print('\n ▲ at ' + str(datetime.datetime.now())) 
+        
         Count = Count + 1
-seconds = time()
-thirds = 0
-while thirds > 120:
-    
+try:
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(6, GPIO.IN)
     GPIO.add_event_detect(6, GPIO.BOTH, callback=my_callback)
-    if thirds == 60:
-        print(count)
-
-    thirds = time() - seconds
+ 
+    message = raw_input('\nPress any key to exit.\n')
+ 
+time.sleep(60)
+print(count)
+GPIO.cleanup()
+ 
+print("Goodbye!")
  
