37fb26641f79bf47896bb2af66db7a6501cabb0e
lol173821 <68711803+lol173821@users.noreply.github.com>
2025-04-04 14:23:42 -0700

    Update Radiation_Sensors.py

--- Radiation_Sensors.py
+++ Radiation_Sensors.py
@@ -1,12 +1,16 @@
 import RPi.GPIO as GPIO
 import datetime
 import time
+import sys
 #write a script that prints out the time-stamp each time a count is detected by your sensor
 #NOTE: you will want to use a falling-edge event detection
 #NOTE: call-back methods are functions that only run when some external property changes, 
 #in this case, the change in voltage on the GPIO pin
 global Count
 Count = 0
+file = open(str(sys.argv[3] + ".csv", "w", newline=None)
+file_writer = csv.writer(file)
+file_writer.writerow(["time", "count"])
 def my_callback(channel):
      print('\n▼  at ' + str(datetime.datetime.now()))
      global Count
@@ -16,10 +20,11 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(6, GPIO.IN)
 GPIO.add_event_detect(6, GPIO.FALLING, callback=my_callback)
 thirds = 0
-while thirds < 120:
-    time.sleep(10)
-    thirds = thirds + 10
+while thirds < int(sys.argv[1]):
+    time.sleep(int(sys.argv[2]))
+    thirds = thirds + int(sys.argv[2])
     print("I'm not dead")
     if thirds == 60:
         print(Count)
+    file_writer.writerow([thirds, Count])
 
